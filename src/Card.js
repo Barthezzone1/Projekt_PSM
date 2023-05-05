01afc98867d69ae67a5a61f3845b12bbd4f1dadb
Barthezzone1 <s219117@student.uek.krakow.pl>
2023-05-06 00:32:35 +0200

    final

--- src/Card.js
+++ src/Card.js
@@ -14,12 +14,12 @@ export default function Card(props) {
           <div className="card_img" style={{ "backgroundImage": image }}></div>
         </Link>
         <div className="cardInfo">
-          <h1>{title}</h1>
+          <h1 style={{ cursor: "default" }}>{title}</h1>
           <div className="action">
             <div className="priceGroup">
-              <p className="price">{price}</p>
+              <p className="price" style={{ cursor: "default" }}>{price}</p>
             </div>
-            <div className="cart">
+            <div className="cart" style={{ cursor: "pointer" }}>
               <svg className="outCart" xmlns="<http://www.w3.org/2000/svg>" viewBox="0 0 64 64" onClick={onAddToBasket}>
                 <path d="M2 6h10l10 40h32l8-24H16"></path>
                 <circle cx="23" cy="54" r="4"></circle>
--- src/History.js
+++ src/History.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db, getCurrentUserID } from "./firebase";
+import { Container, ListGroup } from 'react-bootstrap';
+
 
 const History = () => {
   const [orders, setOrders] = useState([]);
@@ -16,17 +18,28 @@ const History = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Order history</h1>
-      <ul>
+    <Container className="">
+      <p class="text-center">
+        <h1>
+          Order history
+        </h1>
+      </p>
+      <ListGroup>
         {orders.map((order) => (
-          <li key={order}>
-            <p>Items: {order.items}</p>
-            <p>Total: {order.total}</p>
-          </li>
+          <ListGroup.Item key={order.id}>
+            <h4>Items:</h4>
+            <ListGroup>
+              {order.items.map((item) => (
+                <ListGroup.Item key={item.name}>
+                  {item.name}: ${item.price}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+            <h5>Total: ${order.total}</h5>
+          </ListGroup.Item>
         ))}
-      </ul>
-    </div>
+      </ListGroup>
+    </Container>
   );
 };
 
